76399ec7cd856b66aa9265b591448bb796313ec8
Dmitriy An <pancake0890@mail.ru>
2019-09-29 16:34:04 +0500

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = 0;
+        this.max = max;
     }
 
     guess() {
-
+        this.number = Math.round((this.min + this.max) / 2);
+        return this.number;
     }
 
     lower() {
-
+        this.max = this.number;
     }
 
     greater() {
-
+        this.min = this.number;
     }
 }
 
